4295f56d352e853d02d187c57abf6f116f768dfe
Edip Hazuri <edip@medip.dev>
2026-04-23 17:24:46 +0300

    add 003-buttons glade example

---
+++ glade-examples/003-buttons/main.py
@@ -0,0 +1,21 @@
+import gi
+
+gi.require_version("Gtk", "3.0")
+from gi.repository import Gtk
+
+# Create a new window class that inherits from glade's Gtk.Window
+
+class MyWindow():
+    def __init__(self):
+        super().__init__()
+
+        self.builder = Gtk.Builder()
+        self.builder.add_from_file("main.glade")
+
+        self.window = self.builder.get_object("main_window")
+        self.button = self.builder.get_object("my_button")
+        
+
+Application = MyWindow()
+Application.window.show_all()
+Gtk.main()
